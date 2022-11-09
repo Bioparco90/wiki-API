a73b07bc92592a9e4a7a2ee671c851acebf8acea
bioparco90 <monnimarco90@gmail.com>
2022-11-09 17:02:49 +0100

    post articles skeleton

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect('mongodb://localhost:27017/wikiDB');
+mongoose.connect("mongodb://localhost:27017/wikiDB");
 
 const articleSchema = {
-    "title": String,
-    "content": String
-}
+  title: String,
+  content: String,
+};
 
-const Article = mongoose.model('Article', articleSchema);
+const Article = mongoose.model("Article", articleSchema);
 
-app.get('/articles', (req, res) => {
-    Article.find((err, results) => {
-        if (err) {
-            console.log('Error in get articles');
-            res.send(err);
-        }
-        else {
-            console.log('get articles OK');
-            res.send(results);
-        }
-    });
+app.get("/articles", (req, res) => {
+  Article.find((err, results) => {
+    if (err) {
+      console.log("Error in get articles");
+      res.send(err);
+    } else {
+      console.log("get articles OK");
+      res.send(results);
+    }
+  });
+});
+
+app.post("/articles", (req, res) => {
+  const user = req.body.username;
+  const pass = req.body.password;
+  console.log(req.body);
+  res.send(`User: ${user}\nPassword: ${pass}`);
 });
 
 app.listen(3000, () => console.log(`Server is running on port 3000`));
