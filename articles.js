30580b00489a84bc67d0239f8eb82b84bad6e32d
bioparco90 <monnimarco90@gmail.com>
2022-11-11 18:31:29 +0100

    cleaning

--- articles.js
+++ articles.js
@@ -9,39 +9,38 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-// router.param('articleName', (req, res, next) => next());
+router
+  .get("/", (req, res) => {
+    Article.find((err, results) => {
+      err ? res.send(err) : res.send(results);
+    });
+  })
 
-router.get("/", (req, res) => {
-  Article.find((err, results) => {
-    err ? res.send(err) : res.send(results);
+  .get("/:articleName", (req, res) => {
+    let article = req.params.articleName;
+    Article.findOne({ title: article }, (err, result) => {
+      !result ? res.send("No match found") : res.send(result);
+    });
+  })
+
+  .post("/", (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const newArticle = new Article({
+      title: title,
+      content: content,
+    });
+
+    newArticle.save((err) => {
+      err ? res.send(err) : res.redirect("/articles");
+    });
+  })
+
+  .delete("/", (req, res) => {
+    Article.deleteMany({}, (err) => {
+      err ? res.send(err) : res.redirect("/articles");
+    });
   });
-})
-
-.get("/:articleName", (req, res) => {
-  let article = req.params.articleName;
-  Article.findOne({title: article}, (err, result) => {
-    !result ? res.send("No match found") : res.send(result);
-  });
-})
-
-.post("/", (req, res) => {
-  const title = req.body.title;
-  const content = req.body.content;
-
-  const newArticle = new Article({
-    title: title,
-    content: content,
-  });
-
-  newArticle.save((err) => {
-    err ? res.send(err) : res.redirect("/articles");
-  });
-})
-
-.delete("/", (req, res) => {
-  Article.deleteMany({}, (err) => {
-    err ? res.send(err) : res.redirect("/articles");
-  });
-});
 
 module.exports = router;
